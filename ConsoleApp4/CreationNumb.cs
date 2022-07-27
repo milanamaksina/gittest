e13f189f7cb489d36170bbeaa39ff02d280b2fb1
Milana Maksina <m_maksinaa@mail.ru>
2022-07-27 18:33:46 +0300

    first commit

---
+++ ConsoleApp4/CreationNumb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+
+public class CreationNumb
+{
+
+    int[] number = new int[10] { 9, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    public string CreatePhoneNumber(int[] number)
+    {
+
+        string phoneNumb = "";
+
+        foreach (int n in number)
+        {
+            phoneNumb += n.ToString();
+        }
+
+        return string.Format("{0:(###) ###-####}", Convert.ToInt64(phoneNumb));
+
+    }
+=
+}
+
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+int[] number = new int[] { 9, 8, 1, 4, 5, 2, 3, 7, 0, 6};
+
+CreationNumb CreationNumb = new CreationNumb();
+
+string ans = CreationNumb.CreatePhoneNumber(number);
+
+Console.WriteLine(ans);
